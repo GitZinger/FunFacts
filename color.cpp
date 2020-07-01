24ed23aa29030fe60931a50fbb1183296a875f53
GitZinger <785783434@qq.com>
2020-07-01 19:21:29 -0400

    Create color.cpp
    gross

---
+++ color.cpp
@@ -0,0 +1,2 @@
+cout<<"For HSV, hue range is [0,179], saturation range is [0,255], and value range is [0,255]. 
+Different software use different scales. So if you are comparing OpenCV values with them, you need to normalize these ranges."<<endl;
