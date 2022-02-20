783724c45ebc5b5e367149e7e2460fcdd89c5d06
GitZinger <yulong778077@gmail.com>
2022-02-20 17:26:38 -0500

    Update SBT.sh

--- SBT.sh
+++ SBT.sh
@@ -12,3 +12,4 @@ $ source ~/.bashrc
 # now you just type sbt anywhere
 
 $ sbt -java-home <path/to/jdk>  # to run with another jdk
+# so when sbt is running, it will load the build.sbt file. in there scala version and sbt version were specified. and sbt will auto download that version for you.
