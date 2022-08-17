10705a5b10c00f63dedb38c6e18ee8dc1031bfae
GitZinger <yulong778077@gmail.com>
2022-08-17 14:56:51 -0400

    Update and rename OpencvHSV.cpp to OpenCV.cpp

--- OpenCV.cpp
+++ OpenCV.cpp
@@ -1,2 +1,7 @@
+
+//HARR haarcascade anecodote
+std::cout << " all the haarcascade_xxx.xml are located in opencv\build\etc\haarcascades if you have the source project"std::<<endl;
+
+//HSV anecodote
 cout<<"For HSV, hue range is [0,179], saturation range is [0,255], and value range is [0,255]. 
 Different software use different scales. So if you are comparing OpenCV values with them, you need to normalize these ranges."<<endl;
