6ef3df2565cad332b3134c4c0876b85289ae31e0
GitZinger <yulong778077@gmail.com>
2023-05-30 14:20:38 -0400

    Create setupIntelliJ.java

---
+++ setupIntelliJ.java
@@ -0,0 +1,10 @@
+public class test {
+
+    public static void main(String[] args) {
+
+        System.out.println("for java + scala mixed project from scratch"); // 
+        System.out.println(" you only mark the java and scala under the src/main as the working root and !!!!"); // 
+        System.out.println("and you need to add the same name under the src/test to make it work; no other working root !!!!"); // 
+
+    }
+}
